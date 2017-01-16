4f1b5ea0aefbbb8a342d81ee370d3ee0b1fa4a9a
joeyism <sham.joey@gmail.com>
2017-01-16 18:15:51 -0500

    parallelize and first commit

--- house_prices.R
+++ house_prices.R
@@ -1,4 +1,12 @@
 library(randomForest)
+library(foreach)
+library(doParallel)
+
+# Parallel stuff
+cl <- makeCluster(4)
+registerDoParallel(cl)
+
+# Reading Data
 test = read.csv("test.csv")
 train = read.csv("train.csv")
 
@@ -10,13 +18,16 @@ train$Alley[is.na(train$Alley)] <- "None"
 
 test$LotFrontage[is.na(test$LotFrontage)] <- median(test$LotFrontage, na.rm = TRUE)
 levels(test$Alley) <- c(levels(test$Alley), "None")
+levels(test$Condition2) <- levels(train$Condition2)
 test$Alley[is.na(test$Alley)] <- "None"
-
+test$Utilities[is.na(test$Utilities)] <- "AllPub" # Missing Utilities is just AllPub
+test$MSZoning[is.na(test$MSZoning)] <- "RL" # Set missing MSZoning to RL
 
 
 # RFing
 print("Random Forest-ing")
-my_forest = randomForest(as.factor(SalePrice) ~ MSSubClass + MSZoning + LotArea + LotConfig + Neighborhood + Condition1 + Condition2 + OverallQual + OverallCond + YearRemodAdd, data = train, importance = TRUE, ntree = 200)
+#my_forest <- randomForest(as.factor(SalePrice) ~ MSSubClass + MSZoning + LotArea + LotConfig + Neighborhood + Condition1 + Condition2 + OverallQual + OverallCond + YearRemodAdd, data = train, importance = TRUE, ntree = 200)
+my_forest <- foreach(ntree=rep(50, 4), .combine=combine, .packages="randomForest") %dopar%  randomForest(as.factor(SalePrice) ~ MSSubClass + MSZoning + LotArea + LotConfig + Neighborhood + Condition1 + Condition2 + OverallQual + OverallCond + YearRemodAdd, data = train, importance = TRUE, ntree = ntree)
 
 print("Predicting")
 my_prediction <- predict(my_forest, test)
